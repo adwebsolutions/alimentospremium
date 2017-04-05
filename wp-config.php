625cfd70f61a70a869b9efd7a2025d5111011c71
Valentin Magaña <valentin.magana@adweb.mx>
2017-04-05 11:09:50 -0500

    Sitio Actualizado
    -Versión del sitio actualizada

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'barfbarf');
+define('DB_NAME', 'desarr14_alimentosp');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'desarr14');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'A478MZqrS!0');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
